3c5ff778a34130792b60c018c720e39c6022382b
Felipe Goyes <f.goyes@it-frankfurt.com>
2023-05-04 15:50:39 +0200

    Convert wizard information to TeamMatch command

--- src/WebUI/ClientApp/src/app/team-match.service.ts
+++ src/WebUI/ClientApp/src/app/team-match.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Players } from './models/Players';
+import { WizardInformation } from './models/WizardInformation';
+import { CreateTeamMatchCommand, DoublePosition, PlayerRequest, TeamRequest } from './web-api-client';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,46 @@ export class TeamMatchService {
   constructor() { }
 
 
-  // public save(wizardInformation: WizardInformation){
+  public save(wizardInformation: WizardInformation){
+    var teamMatchRequest = this.ToRequest(wizardInformation);
+  }
 
-  // }
+  private ToRequest(wizardInformation: WizardInformation): CreateTeamMatchCommand{
+
+    var teamMatchCommand = new CreateTeamMatchCommand();
+    teamMatchCommand.hostTeam = this.CreateTeamRequest(
+      wizardInformation.teamInformation.hostName, 
+      wizardInformation.hostPlayers,
+      wizardInformation.hostFirstDouble,
+      wizardInformation.hostSecondDouble);
+
+    teamMatchCommand.guestTeam = this.CreateTeamRequest(
+      wizardInformation.teamInformation.guestName, 
+      wizardInformation.guestPlayers,
+      wizardInformation.guestFirstDouble,
+      wizardInformation.guestSecondDouble);
+
+    return teamMatchCommand;
+  }
+
+  private CreateTeamRequest(name: string, players: Players, firstDoubleNames: string[], secondDoubleNames: string[]){
+    return new TeamRequest({name: name, players: [
+      new PlayerRequest({fullName: players.player1, doublePosition: this.GetDoublePosition(players.player1, firstDoubleNames, secondDoubleNames)}),
+      new PlayerRequest({fullName: players.player2, doublePosition: this.GetDoublePosition(players.player2, firstDoubleNames, secondDoubleNames)}),
+      new PlayerRequest({fullName: players.player3, doublePosition: this.GetDoublePosition(players.player3, firstDoubleNames, secondDoubleNames)}),
+      new PlayerRequest({fullName: players.player4, doublePosition: this.GetDoublePosition(players.player4, firstDoubleNames, secondDoubleNames)}),
+    ]})
+  }
+
+  private GetDoublePosition(playerName: string, firstDoubleNames: string[], secondDoubleNames: string[]): DoublePosition{
+    if(firstDoubleNames.includes(playerName)){
+      return DoublePosition.FirstDouble
+    }
+
+    if(secondDoubleNames.includes(playerName)){
+      return DoublePosition.SecondDouble;
+    }
+
+    return DoublePosition.None;
+  }
 }
