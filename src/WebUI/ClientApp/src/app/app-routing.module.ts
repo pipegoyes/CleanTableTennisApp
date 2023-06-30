8a376c672a234d66a32d631b81542a842bfb9427
Felipe Goyes <f.goyes@it-frankfurt.com>
2023-06-30 22:22:50 +0200

    Delete old elements frontend

--- src/WebUI/ClientApp/src/app/app-routing.module.ts
+++ src/WebUI/ClientApp/src/app/app-routing.module.ts
@@ -2,24 +2,17 @@ import { compileClassMetadata } from '@angular/compiler';
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
-import { CounterComponent } from './counter/counter.component';
 import { DoubleScoresComponent } from './double-scores/double-scores.component';
 import { DoublesComponent } from './doubles/doubles.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { GuestPlayersComponent } from './guest-players/guest-players.component';
 import { HomeComponent } from './home/home.component';
 import { HostPlayersComponent } from './host-players/host-players.component';
 import { OverviewComponent } from './overview/overview.component';
-import { PlayersComponent } from './players/players.component';
 import { QuickViewComponent } from './quick-view/quick-view.component';
 import { ResumeComponent } from './resume/resume.component';
-import { ScoreService } from './score.service';
 import { ScoresComponent } from './scores/scores.component';
 import { StartWizardComponent } from './start-wizard/start-wizard.component';
 import { TeamInformationComponent } from './team-information/team-information.component';
-import { TeamMatchConfigurationComponent } from './team-match-configuration/team-match-configuration.component';
-import { TodoComponent } from './todo/todo.component';
-import { TokenComponent } from './token/token.component';
 
 export const routes: Routes = [
 
@@ -27,7 +20,6 @@ export const routes: Routes = [
   { path: 'overview/:teamMatchId/single-scores/:matchId', component: ScoresComponent},
   { path: 'overview/:teamMatchId/double-scores/:doubleMatchId', component: DoubleScoresComponent},
   { path: 'quick-view/:teamMatchId', component: QuickViewComponent},
-  { path: 'match-config', component: TeamMatchConfigurationComponent },
   { path: 'start-wizard', component: StartWizardComponent, children: [
     {
       path: '',
@@ -57,8 +49,8 @@ export const routes: Routes = [
   ]
 },
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'todo', component: TodoComponent, canActivate: [AuthorizeGuard] },
-  { path: 'token', component: TokenComponent, canActivate: [AuthorizeGuard] }
+  // { path: 'todo', component: TodoComponent, canActivate: [AuthorizeGuard] },
+  // { path: 'token', component: TokenComponent, canActivate: [AuthorizeGuard] }
 ];
 
 @NgModule({
--- src/WebUI/ClientApp/src/app/app.module.ts
+++ src/WebUI/ClientApp/src/app/app.module.ts
@@ -7,16 +7,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TodoComponent } from './todo/todo.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
-import { TokenComponent } from './token/token.component';
-import { TeamMatchConfigurationComponent } from './team-match-configuration/team-match-configuration.component';
 import { TabsModule } from 'ngx-bootstrap';
 import { PlayersConfigComponent } from './players-config/players-config.component';
 import { StepsModule } from 'primeng/steps';
@@ -25,7 +20,6 @@ import {CardModule } from 'primeng/card';
 import { StartWizardComponent } from './start-wizard/start-wizard.component';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
-import { PlayersComponent } from './players/players.component';
 import { HostPlayersComponent } from './host-players/host-players.component';
 import { GuestPlayersComponent } from './guest-players/guest-players.component';
 import { DoublesComponent } from './doubles/doubles.component';
@@ -49,15 +43,9 @@ import { QuickViewComponent } from './quick-view/quick-view.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    TodoComponent,
-    TokenComponent,
-    TeamMatchConfigurationComponent,
     PlayersConfigComponent,
     TeamInformationComponent,
     StartWizardComponent,
-    PlayersComponent,
     HostPlayersComponent,
     GuestPlayersComponent,
     DoublesComponent,
@@ -92,7 +80,6 @@ import { QuickViewComponent } from './quick-view/quick-view.component';
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true },
     { provide: API_BASE_URL, useValue: environment.apiUrl },
-    // MessageService
   ],
   bootstrap: [AppComponent]
 })
