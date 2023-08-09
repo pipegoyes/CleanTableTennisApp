29b97569ee05cbddfa5e49e545a502f9c5ae5a9f
Felipe Goyes <f.goyes@it-frankfurt.com>
2023-08-09 14:51:11 +0200

    Create db using terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_windows_web_app" "table_tennis_webapp" {
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLServer"
-    value = "Data Source=myserver.database.windows.net;Database=CleanTableTennisAppDb;User=sa;Password=Your_password123;MultipleActiveResultSets=true;"
+    value = "Server=tt-db.database.windows.net;Database=CleanTableTennisAppDb;User=XXX;Password=XXX;MultipleActiveResultSets=true"
   }
 
   app_settings = {
@@ -56,14 +56,38 @@ resource "azurerm_windows_web_app" "table_tennis_webapp" {
 }
 
 #  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_windows_web_app.table_tennis_webapp.id
-  repo_url               = "https://github.com/pipegoyes/CleanTableTennisApp.git"
-  branch                 = "master"
-  use_manual_integration = true
-  use_mercurial          = false
+# resource "azurerm_app_service_source_control" "sourcecontrol" {
+#   app_id                 = azurerm_windows_web_app.table_tennis_webapp.id
+#   repo_url               = "https://github.com/pipegoyes/CleanTableTennisApp.git"
+#   branch                 = "master"
+#   use_manual_integration = true
+#   use_mercurial          = false
+# }
+
+# resource "azurerm_static_site" "front-end" {
+#   name = "front-end"
+#   resource_group_name = azurerm_resource_group.app_rg.name
+#   location = azurerm_resource_group.app_rg.location
+# }
+
+
+# resource "azurerm_storage_account" "storageacount" {
+#   name                     = "storagedb"
+#   resource_group_name      = azurerm_resource_group.app_rg.name
+#   location                 = azurerm_resource_group.app_rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
+resource "azurerm_mssql_server" "server" {
+  name                         = "random-pet"
+  resource_group_name          = azurerm_resource_group.app_rg.name
+  location                     = azurerm_resource_group.app_rg.location
+  administrator_login          = "XXX"
+  administrator_login_password = "XXX"
+  version                      = "12.0"
 }
 
-# output "app_service_default_hostname" {
-#   value = azurerm_app_service.app_service.default_site_hostname
-# }
+resource "azurerm_mssql_database" "db" {
+  name      = "CleanTableTennisAppDb"
+  server_id = azurerm_mssql_server.server.id
+}
